c8940af2ef11e8b973957b435761c75bf6d0bfc6
0sorojo <redrootproductions@gmail.com>
2020-08-04 10:42:53 -0400

    notes from mithc

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ document.querySelectorAll(".btn").forEach((item) => {
   });
 });
 
+// can use Number() method instead of parseFloat in line 11.
+// use object literal instead of "" in line 14.
+// 'Total: $${totalMoney}';
+
 // ***************** #2 *******************
 
 let coinForm = document.querySelector(".coin-form");
@@ -39,8 +43,12 @@ coinForm.addEventListener("submit", (event) => {
     cloneDiv.innerText = coin;
     cloneDiv.addEventListener("click", removeCoin);
   }
+  coinForm.reset();
 });
 
+// added coinForm.reset() so that it can reset the form each time through
+// line 42 can be changed to use .classlist method;
+// cloneDiv.classlist.add(coin, "coin");
 
 // ***************** #3 *******************
 
@@ -51,28 +59,32 @@ let toggleButton = document.querySelector(".toggle");
 let endButton = document.querySelector(".end");
 
 const turnOn = () => {
- lightBulb.classList.add("light");
- lightBulb.classList.remove("dark");
-}
+  lightBulb.classList.add("light");
+  lightBulb.classList.remove("dark");
+};
 
 const turnOff = () => {
   lightBulb.classList.add("dark");
- lightBulb.classList.remove("light");
-}
+  lightBulb.classList.remove("light");
+};
 
 const toggley = () => {
   lightBulb.classList.toggle("light");
   lightBulb.classList.toggle("dark");
-}
+};
 
 const destroy = () => {
   lightBulb.remove();
   document.querySelectorAll(".button").forEach((item) => {
     item.classList.remove("button");
-});
-}
+    item.disabled = true;
+  });
+};
 
 onButton.addEventListener("click", turnOn);
 offButton.addEventListener("click", turnOff);
 toggleButton.addEventListener("click", toggley);
 endButton.addEventListener("click", destroy);
+
+// the order of the css will determine if it can be changed. the light needs to come after the dark in the css so that it can be applied on or removed.
+// line 80 is added to disable the buttons
